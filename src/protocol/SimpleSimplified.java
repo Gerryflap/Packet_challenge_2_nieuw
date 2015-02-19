7fa49ec6e3be4b73a9bd80e06179d772f8f8c800
Gerryflap <gerry2211@gmail.com>
2015-02-19 12:10:17 +0100

    Fixing NoData

--- src/protocol/SimpleSimplified.java
+++ src/protocol/SimpleSimplified.java
@@ -10,9 +10,11 @@ import java.util.Random;
  * @version 05-12-2013
  */
 public class SimpleSimplified implements IMACProtocol {
+    public static final int SKIP_COUNT = 3;
+
     boolean tried = true;
     private int number = -1;
-
+    private int skipNext = 0;
     public SimpleSimplified(){
         number = new Random().nextInt(4);
         System.out.println(number);
@@ -32,34 +34,45 @@ public class SimpleSimplified implements IMACProtocol {
 
         if (previousMediumState == MediumState.Idle){
             if(new Random().nextInt(100) < 25){
-                return new TransmissionInfo(TransmissionType.NoData, (number + 1) % 4);
+                return new TransmissionInfo(TransmissionType.NoData, new Random().nextInt(4));
             }
         }
 
-        if(tried && number == -1){
-            tried = false;
-            if(previousMediumState == MediumState.Succes){
-                number = (controlInformation-1 +4)%4;
-                System.out.println(number);
-            }
-        }
 
 		// No data to send, just be quiet
 		if (localQueueLength == 0) {
-            if( number == controlInformation || (number == -1 &&  new Random().nextInt(100) < 28)) {
+            if( number == controlInformation%4) {
                 tried = true;
-                return new TransmissionInfo(TransmissionType.NoData, (controlInformation + 1) % 4);
+                return new TransmissionInfo(TransmissionType.NoData, (controlInformation + 1) % 4 + 4);
             } else {
+                tried = false;
                 return new TransmissionInfo(TransmissionType.Silent, (controlInformation + 1) % 4);
             }
 		}
 
-		// Randomly transmit with 60% probability
-		if ((controlInformation == number && number != -1) || (number == -1 &&  new Random().nextInt(100) < 25)) {
+        if ((number+1)%4 == (controlInformation)%4){
+            //System.out.println(controlInformation/4 );
+            //System.out.println(controlInformation);
+
+            if(controlInformation/4 == 1) {
+                skipNext = SKIP_COUNT;
+                //System.out.println("Skip next");
+            }
+        }
+		// Randomly transmit with 60% probability nee
+		if (controlInformation%4 == number) {
             //System.out.println("Sent Data");
             tried = true;
-			return new TransmissionInfo(TransmissionType.Data, (controlInformation + 1)%4);
+            if(skipNext != 0){
+                skipNext -= 1;
+                //System.out.println("Skipped next");
+                return new TransmissionInfo(TransmissionType.Data, (controlInformation+2)%4+8);
+
+            } else {
+                return new TransmissionInfo(TransmissionType.Data, (controlInformation + 1)%4);
+            }
 		} else {
+            tried = false;
 			return new TransmissionInfo(TransmissionType.Silent, (controlInformation + 1)%4);
 		}
 
